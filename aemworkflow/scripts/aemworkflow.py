7b7f415ed948f6dd7f837e9148e42ddbe3b40a43
Melyn Roberts <Melyn.Roberts@ga.gov.au>
2025-09-22 09:39:35 +1000

    Try installing github pip package

---
+++ aemworkflow/scripts/aemworkflow.py
@@ -0,0 +1,17 @@
+import sys
+import click
+from aemworkflow.conversion import main as conversion
+
+
+# @cli.command()
+@click.argument("input_directory", type=click.Path(exists=True))
+@click.argument("output_directory", type=click.Path())
+@click.option("--crs", default="28349", help="Coordinate Reference System (default: EPSG:28349)")
+def convert(input_directory, output_directory, crs):
+    """Convert an AEM interpretation file to a different format."""
+    try:
+        conversion(input_directory, output_directory, crs)
+        click.echo(f"Comleted conversion")
+    except Exception as e:
+        click.echo(f"Error: {e}", err=True)
+        sys.exit(1)
