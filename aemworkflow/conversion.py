841c524474683b1ab94b222dc84f2fe45aaa5b45
Melyn Roberts <Melyn.Roberts@ga.gov.au>
2025-09-22 10:13:37 +1000

    Build package

--- aemworkflow/conversion.py
+++ aemworkflow/conversion.py
@@ -358,19 +358,19 @@ def interpol(col_1: float, frst: int, last: int, tdf: pd.DataFrame) -> Tuple[flo
     return x, y, t
 
 
-def main():
+def main(input_directory: str, output_directory: str, crs: str) -> None:
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        ap = argparse.ArgumentParser()
-        ap.add_argument("--input_directory", "-i", required=True, help="Input directory with path and extent files")
-        ap.add_argument("--output_directory", "-o", required=True, help="Output directory for generated files")
-        ap.add_argument("--crs", "-c", required=False, help="Defaults to (GDA94 / MGA zone 49) EPSG:28349")
+        # ap = argparse.ArgumentParser()
+        # ap.add_argument("--input_directory", "-i", required=True, help="Input directory with path and extent files")
+        # ap.add_argument("--output_directory", "-o", required=True, help="Output directory for generated files")
+        # ap.add_argument("--crs", "-c", required=False, help="Defaults to (GDA94 / MGA zone 49) EPSG:28349")
 
-        ARG = vars(ap.parse_args())
+        # ARG = vars(ap.parse_args())
 
-        input_directory = ARG["input_directory"]
-        output_directory = ARG["output_directory"]
-        crs = ARG["crs"] if ARG["crs"] else 28349
+        # input_directory = ARG["input_directory"]
+        # output_directory = ARG["output_directory"]
+        # crs = ARG["crs"] if ARG["crs"] else 28349
         work_dir = output_directory
         path_dir = input_directory
 
