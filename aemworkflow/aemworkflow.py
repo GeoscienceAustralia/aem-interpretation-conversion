833abff99b0a6bd09bbb33b42a7a7ec0d145f73e
Melyn Roberts <Melyn.Roberts@ga.gov.au>
2025-09-29 14:44:53 +1000

    Make directories required

--- aemworkflow/aemworkflow.py
+++ aemworkflow/aemworkflow.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import click
 from .conversion import main as conversion
@@ -12,8 +13,8 @@ def cli():
     pass
 
 @cli.command()
-@click.option("--i", "input_directory", type=click.Path(exists=True))
-@click.option("--o", "output_directory", type=click.Path())
+@click.option("--i", "input_directory", type=click.Path(exists=True), required=True)
+@click.option("--o", "output_directory", type=click.Path(), required=True)
 @click.option("--crs", default="28349", help="Coordinate Reference System (default: EPSG:28349)")
 @click.option("--gis", default="esri_arcmap_0.5", help="GIS format (default: esri_arcmap_0.5)")
 @click.option("--lines", default=10, help="Number of depth lines (default: 10)")
@@ -27,8 +28,8 @@ def pre_interpret(input_directory, output_directory, crs, gis="esri_arcmap_0.5",
         sys.exit(1)
 
 @cli.command()
-@click.option("--i", "input_directory", type=click.Path(exists=True))
-@click.option("--o", "output_directory", type=click.Path())
+@click.option("--i", "input_directory", type=click.Path(exists=True), required=True)
+@click.option("--o", "output_directory", type=click.Path(), required=True)
 @click.option("--crs", default="28349", help="Coordinate Reference System (default: EPSG:28349)")
 @click.option("--gis", default="esri_arcmap_0.5", help="GIS format (default: esri_arcmap_0.5)")
 @click.option("--lines", default=10, help="Number of depth lines (default: 10)")
@@ -42,9 +43,9 @@ def interpret(input_directory, output_directory, crs="28349", gis="esri_arcmap_0
         sys.exit(1)
 
 @cli.command()
-@click.option("--i", "input_directory", type=click.Path(exists=True))
-@click.option("--o", "output_directory", type=click.Path())
-@click.option("--a", "asud_filename", type=click.Path(exists=True))
+@click.option("--i", "input_directory", type=click.Path(exists=True), required=True)
+@click.option("--o", "output_directory", type=click.Path(), required=True)
+@click.option("--a", "asud_filename", type=click.Path(exists=True), required=True)
 def validate(input_directory, output_directory, asud_filename):
     try:
         validation(input_directory, output_directory, asud_filename)
@@ -54,8 +55,8 @@ def validate(input_directory, output_directory, asud_filename):
         sys.exit(1)
 
 @cli.command()
-@click.option("--i", "input_directory", type=click.Path(exists=True))
-@click.option("--o", "output_directory", type=click.Path())
+@click.option("--i", "input_directory", type=click.Path(exists=True), required=True)
+@click.option("--o", "output_directory", type=click.Path(), required=True)
 @click.option("--crs", default="28349", help="Coordinate Reference System (default: EPSG:28349)")
 def convert(input_directory, output_directory, crs):
     try:
@@ -66,10 +67,10 @@ def convert(input_directory, output_directory, crs):
         sys.exit(1)
 
 @cli.command()
-@click.option("--i", "input_directory", type=click.Path(exists=True))
-@click.option("--o", "output_directory", type=click.Path())
-@click.option("--b", "boundary_filename", type=click.Path(exists=True))
-@click.option("--s", "split_filename", type=click.Path(exists=True))
+@click.option("--i", "input_directory", type=click.Path(exists=True), required=True)
+@click.option("--o", "output_directory", type=click.Path(), required=True)
+@click.option("--b", "boundary_filename", type=click.Path(exists=True), required=True)
+@click.option("--s", "split_filename", type=click.Path(exists=True), required=True)
 @click.option("--mdc", is_flag=True, help="Export to MDC format", default=False)
 @click.option("--mdch", is_flag=True, help="Export to MDCH format", default=False)
 @click.option("--egs", is_flag=True, help="Export to EGS format", default=False)
